f71f35544c1be98fe74be2f10078723bb271e0e4
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-06-23 13:11:27 -0300

    exemploa ula 14

--- JavaScript-Avançado/node/aula14/serve.js
+++ JavaScript-Avançado/node/aula14/serve.js
@@ -1,8 +1,23 @@
+require('dotenv').config() // importando o dotenv para pegar a senha do arquivo .env
+// imports
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+//conectios mongodb and mongoose
+
+mongoose.connect(process.env.conectionString, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('conectei á base de dados')
+        app.emit('pronto') // emitir uma evento quando estiver pronto
+    })
+    .catch((e) => console.log(e))
+
+
 const routes = require('./routes')
 const path = require('path')
 
+// app users files
 app.use(express.urlencoded({ extended: true})) // serve para o req.body funcionar
 
 app.use(express.static(path.resolve(__dirname, 'public'))) // usar a pasta de conteudo statico "os css imagens e etc..."
@@ -13,6 +28,7 @@ app.set('view engine', 'ejs')
 
 app.use(routes)// aqui eu falo para o app usar as minhas rotas que eu separei
 
-
-
-app.listen(3003, () => console.log(' http://localhost:3003  rodando com sucesso'))
+// escutar na porta
+app.on('pronto', ()=> { // quando o evento estiver pronto execultar!
+    app.listen(3003, () => console.log(' http://localhost:3003  rodando com sucesso'))
+})
